269577d3f1287e5ef3f935e73144fe73c337d825
Abhi123sh <101368684+Abhi123sh@users.noreply.github.com>
2023-07-18 10:04:36 -0400

    Issue #41 | feat: candidate_registration_backend

---
+++ backend/src/main/java/com/sep/backend/models/Candidate.java
@@ -0,0 +1,46 @@
+package com.sep.backend.models;
+
+import com.sep.backend.enums.Gender;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+/**
+ * The type Candidate.
+ */
+@Entity
+@Table(name = "candidate")
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Candidate extends BaseModel{
+
+@Column(name = "first_name")
+private String firstName;
+
+@Column(name = "last_name", nullable = false)
+private String lastName;
+
+@Column(name = "middle_name")
+private String middleName;
+
+@Column(name = "phone_number", unique = true)
+private String phoneNumber;
+
+@Column(name = "email_address", unique = true)
+private String emailAddress;
+
+@Column(name = "gender", nullable = false)
+private Gender gender;
+
+@Column(name = "age", nullable = false)
+private Integer age;
+
+@Column(name = "password", nullable = false)
+private String password;
+
+}
