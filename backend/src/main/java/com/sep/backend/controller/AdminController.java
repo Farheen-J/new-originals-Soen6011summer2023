83798b847aa5054b118c8739daaa5b9d4a9f4a8c
Abhi123sh <101368684+Abhi123sh@users.noreply.github.com>
2023-08-08 23:28:24 -0400

    Issue #181 | Developed API to get a report of the current stats of the applicaiton

--- backend/src/main/java/com/sep/backend/controller/AdminController.java
+++ backend/src/main/java/com/sep/backend/controller/AdminController.java
@@ -3,6 +3,7 @@ package com.sep.backend.controller;
 import com.sep.backend.constants.UriConstants;
 
 import com.sep.backend.dto.AdminJobTrackingResponseDto;
+import com.sep.backend.dto.AdminReport;
 import com.sep.backend.dto.ResponseDto;
 import com.sep.backend.models.AdminTrackJob;
 import com.sep.backend.service.IAdminJobTrackingService;
@@ -22,15 +23,18 @@ import java.util.Collections;
 @RequestMapping(UriConstants.BASE_URL)
 public class AdminController {
 
-    private final IAdminJobTrackingService iAdminService;
+    private final IAdminJobTrackingService iAdminJobTrackingService;
+
+    private final IAdminService iAdminService;
 
     /**
      * Instantiates a new  controller.
      *
-     * @param iAdminService the  service
+     * @param iAdminJobTrackingService the  service
      */
     @Autowired
-    public AdminController(IAdminJobTrackingService iAdminService){
+    public AdminController(IAdminJobTrackingService iAdminJobTrackingService, IAdminService iAdminService){
+        this.iAdminJobTrackingService = iAdminJobTrackingService;
         this.iAdminService = iAdminService;
     }
 
@@ -40,7 +44,7 @@ public class AdminController {
 
         AdminTrackJob adminTrackJob;
 
-        adminTrackJob = iAdminService.findAllJobs();
+        adminTrackJob = iAdminJobTrackingService.findAllJobs();
         if(adminTrackJob == null)
         {
             return new ResponseDto<>(Collections.singletonList("No Jobs to track for admin!"));
@@ -56,6 +60,17 @@ public class AdminController {
 
         return new ResponseDto<>(responseDto);
 
+    }
+
+    @RequestMapping(method=RequestMethod.GET, value = UriConstants.ADMIN_REPORT)
+    public ResponseDto<AdminReport> getReport(){
+        try{
+            AdminReport adminReport= iAdminService.getAdminReport();
+            return new ResponseDto<>(adminReport);
+        }catch (Exception ex){
+            log.error("Error occurred ::", ex );
+            return new ResponseDto<>(Collections.singletonList("Some Error Occurred"));
+        }
     }
 
 }
--- backend/src/main/java/com/sep/backend/service/IAdminService.java
+++ backend/src/main/java/com/sep/backend/service/IAdminService.java
@@ -1,9 +1,10 @@
 package com.sep.backend.service;
 
+import com.sep.backend.dto.AdminReport;
 import com.sep.backend.dto.LoginResponse;
 
 import com.sep.backend.exception.LoginException;
-
+import com.sep.backend.models.Admin;
 
 
 /**
@@ -14,5 +15,8 @@ public interface IAdminService {
 
 
     LoginResponse getLoginDetails(String email, String password) throws LoginException;
+
+
+    AdminReport getAdminReport();
 
 }
